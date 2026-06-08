d086b4dffdadd8bc4806e68fc268a2d10e47e473
tonkusinaanna-sudo <tonkusinaanna@gmail.com>
2026-06-08 22:22:33 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -213,5 +213,6 @@ def serve_index():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
